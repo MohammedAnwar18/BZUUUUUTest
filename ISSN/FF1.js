d96cb944d378db89f9789fe1d05b6bcf84f65523
MohammedAnwar18 <105022929+MohammedAnwar18@users.noreply.github.com>
2025-05-22 21:42:31 +0300

    Add files via upload

---
+++ ISSN/FF1.js
@@ -0,0 +1,6 @@
+// هذا الملف يعرف كل المناطق التي تريد عرضها
+const geojsonFiles = [
+  { name: 'دير ياسين', file: 'EX1111.geojson' },
+  { name: 'المنطقة 2', file: 'area2.geojson' },
+  { name: 'المنطقة 3', file: 'area3.geojson' }
+];
